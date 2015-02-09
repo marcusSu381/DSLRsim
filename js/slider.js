32e292262847d356b90ab3e10914522da465f5ff
marcus <marcusssu@gmail.com>
2015-02-08 21:52:46 -0800

    final with try it button

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-var output = document.querySelectorAll('output')[0];
-  
-  // $(document).on('change', 'input[type="range"]', function(e) {
-  //       output.innerHTML = e.target.value+".jpg";
-  // });
-  
-  $('input[type=range]').rangeslider({
-    polyfill: false
-  });
